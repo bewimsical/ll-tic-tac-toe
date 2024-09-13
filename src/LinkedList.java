54d446b715b74a04150d9a95944f8c4f93a83c22
Kathleen Wims <bewimsical@gmail.com>
2024-09-13 12:29:18 -0400

    initial commit

---
+++ src/LinkedList.java
@@ -0,0 +1,48 @@
+
+public class LinkedList {
+
+    private Node head;  // Head of the linked list
+
+    // Constructor to initialize an empty linked list
+    public LinkedList() {
+        this.head = null;
+    }
+
+    // Method to insert a new node at the end
+    public void insertAtEnd(String data) {
+        // To be implemented
+    }
+
+    // Method to update a specific position in the list
+    public void updatePosition(int position, String data) {
+        // To be implemented
+    }
+
+    // Method to get the value at a specific position
+    public String getPositionValue(int position) {
+        // To be implemented
+        return "";
+    }
+
+    // Method to display the Tic Tac Toe board
+    public void displayBoard() {
+        // To be implemented
+    }
+
+    // Method to check for a win
+    public boolean checkWinCondition() {
+        // To be implemented
+        return false;
+    }
+
+    // Method to check if the board is full (for draw condition)
+    public boolean isBoardFull() {
+        // To be implemented
+        return false;
+    }
+
+    // Method to reset the board
+    public void resetBoard() {
+        // To be implemented
+    }
+}
