1e9a8fb4f9fe8f9f3c631e3c64c52a457422b63a
Kathleen Wims <bewimsical@gmail.com>
2024-09-13 13:02:34 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ll-tic-tac-toe
-console-based Tic Tac Toe game where the game board and moves are managed using a singly linked list data structure.
+# Overview
+This project contains Java code that demonstrates a console-based Tic Tac Toe game where the game board and moves are managed using a singly linked list data structure. Follow the instructions below to compile and run the Java code.
+# Prerequisites
+Java Development Kit (JDK): Ensure that you have JDK 8 or higher installed on your system. You can download it from the official Oracle website or use an open-source alternative like OpenJDK.
+# Additional Information
+IDE Support: This project can be imported into IDEs such as IntelliJ IDEA or Eclipse. These IDEs provide built-in tools for compiling and running Java code.
+
--- src/LinkedList.java
+++ src/LinkedList.java
@@ -10,39 +10,39 @@ public class LinkedList {
 
     // Method to insert a new node at the end
     public void insertAtEnd(String data) {
-        // To be implemented
+        // TODO:To be implemented
     }
 
     // Method to update a specific position in the list
     public void updatePosition(int position, String data) {
-        // To be implemented
+        // TODO: To be implemented
     }
 
     // Method to get the value at a specific position
     public String getPositionValue(int position) {
-        // To be implemented
+        // TODO: To be implemented
         return "";
     }
 
     // Method to display the Tic Tac Toe board
     public void displayBoard() {
-        // To be implemented
+        // TODO: To be implemented
     }
 
     // Method to check for a win
     public boolean checkWinCondition() {
-        // To be implemented
+        // TODO: To be implemented
         return false;
     }
 
     // Method to check if the board is full (for draw condition)
     public boolean isBoardFull() {
-        // To be implemented
+        // TODO: To be implemented
         return false;
     }
 
     // Method to reset the board
     public void resetBoard() {
-        // To be implemented
+        // TODO: To be implemented
     }
 }
